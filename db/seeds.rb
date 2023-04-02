9fb44aa1fcf95d24f175d08d1cd9dddbcf8f8589
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-02 11:31:04 -0500

    add model for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+world = World.create(name: "usa", location: "north america")
+
+world = World.create(name: "france", location: "europe")
+
+world = World.create(name: "iran", location: "middle east")
+
